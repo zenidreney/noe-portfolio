0301d17fc8f07e2e191d18f2727ab15357333039
Deniz <auo@tuta.io>
2026-02-07 16:35:58 +0100

    added albums page.

--- utils/renderPhotos.js
+++ utils/renderPhotos.js
@@ -4,7 +4,6 @@ export function renderPhotos() {
 	const galleryRoot = document.getElementById("gallery-root");
 
 	imagesArray.forEach((img) => {
-		console.log(img.imgSource);
 		const imgTag = document.createElement("img");
 		imgTag.src = img.imgSource;
 		imgTag.alt = img.alt;
